c1fe6f4558449a629f45e56c36dfa437cd1b2a2f
ahmad9059 <ahmadhassan9059@gmail.com>
2025-10-14 02:03:01 +0500

    favicon changed

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,13 @@
-import { BrainCircuit } from 'lucide-react';
-import Link from 'next/link';
+import { Brain } from "lucide-react";
+import Link from "next/link";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Link href="/" className="flex items-center gap-2 text-lg font-bold tracking-tight text-primary">
-      <BrainCircuit className="h-6 w-6" />
+    <Link
+      href="/"
+      className="flex items-center gap-2 text-lg font-bold tracking-tight text-primary"
+    >
+      <Brain className="h-6 w-6" />
       <span className={className}>Mind Oasis</span>
     </Link>
   );
